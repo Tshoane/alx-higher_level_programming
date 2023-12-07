630a9ee54760433cb3cd133d23ef44f16c53af60
tshoane <respectmasehla@icloud.com>
2023-12-06 16:06:34 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,17 +1,59 @@
-#!/usr/bin/python3
+#include "/usr/include/python3.4/Python.h"
+#include <stdio.h>
 
-def magic_calculation(a, b):
+void print_hexn(const char *str, int n)
+{
+	int i = 0;
 
-    """Match bytecode provided by Holberton School."""
+	for (; i < n - 1; ++i)
+		printf("%02x ", (unsigned char) str[i]);
 
-    from magic_calculation_102 import add, sub
+	printf("%02x", str[i]);
+}
 
-    if a < b:
-        c = add(a, b)
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *clone = (PyBytesObject *) p;
+	int calc_bytes, clone_size = 0;
 
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(clone))
+	{
+		clone_size = PyBytes_Size(p);
+		calc_bytes = clone_size + 1;
 
-    else:
-        return(sub(a, b))
+		if (calc_bytes >= 10)
+			calc_bytes = 10;
+
+		printf("  size: %d\n", clone_size);
+		printf("  trying string: %s\n", clone->ob_sval);
+		printf("  first %d bytes: ", calc_bytes);
+		print_hexn(clone->ob_sval, calc_bytes);
+		printf("\n");
+	}
+	else
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+	}
+}
+
+void print_python_list(PyObject *p)
+{
+	int i = 0, list_len = 0;
+	PyObject *item;
+	PyListObject *clone = (PyListObject *) p;
+
+	printf("[*] Python list info\n");
+	list_len = PyList_GET_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", list_len);
+	printf("[*] Allocated = %d\n", (int) clone->allocated);
+
+	for (; i < list_len; ++i)
+	{
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %d: %s\n", i, item->ob_type->tp_name);
+
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
+	}
+}
